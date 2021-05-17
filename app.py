c631d955708c09443f081692095c28419ffb448f
yoduyodu <wang7ong@gmail.com>
2021-05-17 11:29:47 +0800

    Initial

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+  return "Hello, Arnold."
+
+@app.route('/<int:number>')
+def lucky_draw(number):
+    if number == 1:
+        return 'Oh you are not so lucky'
+    elif number == 2:
+        return 'Geez you are extremely lucky'
+    else:
+        return 'Please try one more time'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
